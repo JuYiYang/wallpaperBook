a5a314bd0a830228b8e6d715dc2a5aa84e7a99d7
mebius杨 <juyiyang@qq.com>
2024-06-13 17:33:17 +0800

    接入ParseServer

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const ParseServer = require("parse-server").ParseServer;
 const config = require("./config");
 
 const bodyParser = require("body-parser");
@@ -9,6 +10,12 @@ app.use(bodyParser.json());
 // 自定义中间件
 app.use(responseMiddleware);
 app.use(crossDomainMiddlewar);
+
+const api = new ParseServer(config);
+(async () => await api.start())();
+
+// 将 Parse API 挂载到 /parse 路径
+app.use("/parse", api.app);
 
 app.listen(config.prot, () => {
   console.log(`${config.prot}已启动`);
