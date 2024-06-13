0d9e5e9cf13b78b28cfc2947104d97f530bb854e
mebius杨 <juyiyang@qq.com>
2024-06-13 17:35:37 +0800

    统一导出

---
+++ router/index.js
@@ -0,0 +1,17 @@
+const Account = require("./account");
+
+const Login = require("./login");
+
+const Role = require("./role");
+
+const Collect = require("./collect");
+
+const Like = require("./like");
+
+module.exports = {
+  Role,
+  Login,
+  Account,
+  Collect,
+  Like,
+};
