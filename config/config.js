048a6526af9124ce5fd2ac594860274d878069b1
Yang Mebius <juyiyang@qq.com>
2024-10-07 14:56:00 +0800

    更改数据库连接

--- config/config.js
+++ config/config.js
@@ -2,7 +2,7 @@ const global_parse__config = {
   appId: "mebius",
   masterKey: "mebius",
   databaseURI:
-    "mongodb+srv://juyiyang6:WgOWMRrhBCGU3sBJ@wallpaperbook-tokyo.uyksh.mongodb.net/?retryWrites=true&w=majority&appName=wallpaperbook-tokyok",
+    "mongodb+srv://juyiyang6:WgOWMRrhBCGU3sBJ@wallpaperbook-tokyo.uyksh.mongodb.net/wallpaperbook?retryWrites=true&w=majority&appName=wallpaperbook-tokyo/",
   // databaseURI: "mongodb://127.0.0.1:27017",
   serverURL: "http://localhost:1337/parse",
   sessionLength: 60 * 60 * 24 * 7,
